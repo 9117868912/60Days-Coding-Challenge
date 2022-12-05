ed6e42d2f9503dc181b699413ae06f46c057fdb3
MD AMANULLAH <113604974+9117868912@users.noreply.github.com>
2022-12-05 18:37:46 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ifelse {
+    public static void main(String args[]){
+        int a,b;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the first number is=");
+        a=input.nextInt();
+        System.out.println("Enter the Second Number is =");
+        b=input.nextInt();
+
+        if(a>=b){
+System.out.println("a greater than b");
+        }
+        else{
+            System.out.println("b greater than a ");
+        }
+    }
+    
+}
