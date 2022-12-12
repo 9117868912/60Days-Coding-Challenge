198f6fbcb34344dcf782690988bea8957e4f0f24
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2022-12-12 21:43:44 +0530

    Add files via upload

---
+++ triangle2.java
@@ -0,0 +1,29 @@
+// print the triangle patten
+// *****
+//  ****
+//   *** 
+//    ** 
+//     *
+import java.util.Scanner;
+public class triangle2
+{
+ 
+    public static void main(String[] args)
+    {
+    Scanner input = new Scanner(System.in); 
+    int n = input.nextInt();
+    for (int i= n; i>= 1; i--)
+    {
+    for (int j=n; j>i;j--)
+    {
+    System.out.print(" ");
+    }
+    for (int k=1;k<=i;k++)
+    {
+    System.out.print("*");
+    }
+    System.out.println("");
+    }
+    
+    }
+    }
