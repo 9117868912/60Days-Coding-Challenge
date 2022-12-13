f5734afe0f37b8bf0a5688bc1543a94a72dadec8
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2022-12-13 22:21:34 +0530

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class  pyramid
+{            
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+          
+        int n=input.nextInt();
+  
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+            System.out.printf("%" + (n - i) * 2 + "s", "");
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%4d", number);
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+  
+    }
+  
+}
+  
+
+    
+
---
+++ reversepyramid.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class reversepyramid
+{
+    public static void main(String[] args)
+{
+    Scanner input = new Scanner(System.in);
+    
+ 
+    int n = input.nextInt();        
+    for (int i= 0; i<= n-1 ; i++)
+    {
+        for (int j=0; j<=i; j++)
+        {
+            System.out.print(" ");
+        }
+        for (int k=0; k<=n-1-i; k++)
+        {
+            System.out.print("*" + " ");
+        }
+        System.out.println();
+    }
+
+ 
+}
+}
