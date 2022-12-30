95df3309441886c1497d738d8519d1f060b0b355
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2022-12-30 21:32:26 +0530

    Add files via upload

---
+++ charAtFun.java
@@ -0,0 +1,10 @@
+// create a String use the charAt function
+public class charAtFun {
+    public static void main(String args[]){
+        String name="Adil rose";
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+    
+}
---
+++ lenFun.java
@@ -0,0 +1,9 @@
+// create a String use a lenght function
+public class lenFun {
+    public static void main(String args[]){
+        String sentance="This is my firend Adil rose";
+        System.out.println(sentance);
+        System.out.println(sentance.length());
+    }
+    
+}
