1a93d70727cf8c650037706efb39c3d6fc62c702
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2022-12-31 21:43:54 +0530

    Add files via upload

---
+++ compareStr.java
@@ -0,0 +1,16 @@
+// Comapare two String using == operator
+
+public class compareStr {
+    public static void main(String args[]){
+        String name1="Adil";
+        String name2="Aman";
+        if(name1==name2){
+            System.out.println("String is equal");
+        }
+        else{
+            System.out.println("String is not equal");
+
+        }
+    }
+    
+}
---
+++ compareToStr.java
@@ -0,0 +1,14 @@
+// Compare two String using CompareTo()
+public class compareToStr {
+    public static void main(String args[]){
+        String name1="Aman";
+        String name2="Adil";
+        if(name1.compareTo(name2)==0){
+            System.out.println("String is equal ");
+        }
+        else{
+            System.out.println("String is not equal ");
+        }
+    }
+    
+}
