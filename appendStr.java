8a60f0f04affe74984d6824a267b5048be56cd93
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-01-01 21:17:15 +0530

    Add files via upload

---
+++ appendStr.java
@@ -0,0 +1,12 @@
+// Create a StringBuilder use append() generate the String 
+public class appendStr {
+    public static void main(String args[]){
+        StringBuilder str=new StringBuilder("A");
+        str.append("d");
+        str.append("i");
+        str.append("l");
+        // str.append("d");
+        System.out.println(str);
+    }
+    
+}
---
+++ withoutAppend.java
@@ -0,0 +1,11 @@
+// without use append() generate String
+public class withoutAppend {
+    public static void main(String arg[]){
+        String str="A";
+        str=str+"d";
+        str=str+"i";
+        str=str+"l";
+        System.out.println(str);
+    }
+    
+}
