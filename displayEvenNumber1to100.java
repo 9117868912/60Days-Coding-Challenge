bbb56f34c53f6dfaf0a205f09473dbc7479fac75
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-01-16 22:04:06 +0530

    Add files via upload

---
+++ displayEvenNumber1to100.java
@@ -0,0 +1,18 @@
+// Java Program to Display Even Numbers From 1 to 100
+public class displayEvenNumber1to100 {
+   
+public static void main(String args[])   
+{  
+int number=100;  
+System.out.print("List of even numbers from 1 to "+number+": ");  
+for (int i=1; i<=number; i++)   
+{  
+if (i%2==0)   
+{  
+System.out.println(i + " ");  
+}  
+}  
+}  
+}  
+    
+
