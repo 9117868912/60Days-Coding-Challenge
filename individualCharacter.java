429f43c2ff79ea263c247a7ed648a05c7ebc42aa
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-01-28 21:38:55 +0530

    Add files via upload

---
+++ individualCharacter.java
@@ -0,0 +1,11 @@
+public class individualCharacter {
+    public static void main(String[] args) {
+        String string = "characters ";
+
+        System.out.println("Individual characters from given string: ");
+
+        for (int i = 0; i < string.length(); i++) {
+            System.out.print(string.charAt(i) + " ");
+        }
+    }
+}
