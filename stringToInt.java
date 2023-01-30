169dfaab4076d706b9bc58a3a49db5e7d5c8bb71
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-01-30 21:34:10 +0530

    Add files via upload

---
+++ stringToInt.java
@@ -0,0 +1,10 @@
+
+// Java Program to demonstrate the conversion of String into int
+public class stringToInt{
+public static void main(String args[]){  
+ 
+String s="200";  
+int i=Integer.parseInt(s);  
+System.out.println(i);  
+}
+}
