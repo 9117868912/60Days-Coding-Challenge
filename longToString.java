9ebc0c77ec5709ca2d4f4b8ab52c5939d06fae1a
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-01-31 21:45:31 +0530

    Add files via upload

---
+++ longToString.java
@@ -0,0 +1,9 @@
+
+// How to convert Long to String 
+public class longToString {
+    public static void main(String args[]){  
+        long i=9993939399L;  
+        String s=String.valueOf(i);  
+        System.out.println(s);  
+        }
+}
