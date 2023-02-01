f188a8d3b5188f577028a8f5e5517b2e5549e14d
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-01 21:34:38 +0530

    Add files via upload

---
+++ floatToString.java
@@ -0,0 +1,9 @@
+
+// How to convert Float to String 
+public class floatToString {
+    public static void main(String args[]) {
+        float f = 12.3F;
+        String s = String.valueOf(f);
+        System.out.println(s);
+    }
+}
---
+++ stringToFloat.java
@@ -0,0 +1,9 @@
+
+// How to convert String to Float
+public class stringToFloat {
+    public static void main(String args[]) {
+        String s = "23.6";
+        float f = Float.parseFloat(s);
+        System.out.println(f);
+    }
+}
