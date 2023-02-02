593406a20da5d09b5776784d906405ae24d7ce90
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-02 22:14:13 +0530

    Add files via upload

---
+++ doubleToString.java
@@ -0,0 +1,10 @@
+
+// How to convert Double to String
+public class doubleToString {
+    public static void main(String args[]){  
+        double d=12.3;  
+        String s=String.valueOf(d);  
+        System.out.println(s);  
+        }
+    
+}
---
+++ stringToDouble.java
@@ -0,0 +1,9 @@
+
+// How to convert String to Double
+public class stringToDouble {
+    public static void main(String args[]){  
+        String s="23.6";  
+        double d=Double.parseDouble(s);  
+        System.out.println(d);  
+        }
+}
