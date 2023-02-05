638b87d49d63854f4744edb6b31f807007f0c906
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-05 21:53:30 +0530

    Add files via upload

---
+++ decimalToHexa.java
@@ -0,0 +1,9 @@
+
+// How to convert the Decimal to Hexadecimal
+public class decimalToHexa {
+    public static void main(String args[]) {
+        System.out.println(Integer.toHexString(10));
+        System.out.println(Integer.toHexString(15));
+        System.out.println(Integer.toHexString(289));
+    }
+}
---
+++ hexaToDecimal.java
@@ -0,0 +1,9 @@
+
+// How to convert the Hexadecimal to Decimal
+public class hexaToDecimal {
+    public static void main(String args[]){  
+        String hex="a";  
+        int decimal=Integer.parseInt(hex,16);  
+        System.out.println(decimal);  
+        }
+}
