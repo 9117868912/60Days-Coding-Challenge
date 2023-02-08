bfe18998633fb2c4258092e728a57c37bc1fcb5a
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-08 21:35:15 +0530

    Add files via upload

---
+++ charToInt.java
@@ -0,0 +1,12 @@
+
+// How to convert the Char to Int
+public class charToInt {
+    public static void main(String[] args) {
+        char ch='a';
+        char num='1';
+        int a=ch;
+        int b=num;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ intToCahr.java
@@ -0,0 +1,12 @@
+// How to  convert the Int to Char
+public class intToCahr {
+    public static void main(String[] args) {
+        int a=65;
+        int b=66;
+        char ch=(char)a;
+        char num=(char)b;
+        System.out.println(ch);
+        System.out.println(num);
+    }
+    
+}
