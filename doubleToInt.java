5e3dbb4d07576f680abff823d0932f6902c5b8b2
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-09 21:57:40 +0530

    Add files via upload

---
+++ doubleToInt.java
@@ -0,0 +1,9 @@
+
+// How to convert the Double to Int
+public class doubleToInt {
+    public static void main(String[] args) {
+        double d = 15.5;
+        int i = (int) d;
+        System.out.println(i);
+    }
+}
---
+++ intToDouble.java
@@ -0,0 +1,9 @@
+
+// How to convert the Int to Double 
+public class intToDouble {
+    public static void main(String[] args) {
+        int i = 15;
+        double d = (int) i;
+        System.out.println(d);
+    }
+}
