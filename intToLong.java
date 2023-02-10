69ea4bb9594f63bb86b9492ca5c12a03d22a67c9
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-10 22:29:36 +0530

    Add files via upload

---
+++ intToLong.java
@@ -0,0 +1,13 @@
+import javax.sound.sampled.SourceDataLine;
+
+// How to convert the Int to Long 
+public class intToLong {
+    public static void main(String[] args) {
+        
+    int a=10;
+    long b=a;
+    System.out.println(b);
+        
+    }
+
+}
---
+++ longToInt.java
@@ -0,0 +1,9 @@
+// How to convert the Long to Int
+public class longToInt {
+    public static void main(String[] args) {
+        long a=10;
+        int b=(int)a;
+        System.out.println(b);
+
+    }
+}
