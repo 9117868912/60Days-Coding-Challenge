b8b856fbd152427a3210c943ccb461ec8c1daaa8
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-12 21:59:12 +0530

    Add files via upload

---
+++ StringToChar.java
@@ -0,0 +1,9 @@
+
+// How to convert the String to Char
+public class StringToChar {
+  public static void main(String[] args) {
+    String s="aman";
+    char c=s.charAt(0);
+    System.out.println("1st Character is :"+c);
+  }  
+}
---
+++ charToString.java
@@ -0,0 +1,9 @@
+
+// How to convert the char to String
+public class charToString {
+    public static void main(String[] args) {
+        char c = 'S';
+        String s = String.valueOf(c);
+        System.out.println("String is :" + s);
+    }
+}
