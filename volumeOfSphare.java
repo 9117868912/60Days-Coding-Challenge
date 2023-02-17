f96591a5ba517f17a2b7566a80d9ddfed7d23356
MD AMANULLAH <113604974+AMANULLAH023HY@users.noreply.github.com>
2023-02-17 22:35:22 +0530

    Add files via upload

---
+++ volumeOfSphare.java
@@ -0,0 +1,15 @@
+
+// Find the Volume of Sphare
+import java.util.Scanner;
+
+public class volumeOfSphare {
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the radius of sphere:");
+        double r = s.nextDouble();
+        double volume = (4 * 22 * r * r * r) / (3 * 7);
+
+        System.out.println("Volume is:" + volume);
+    }
+}
